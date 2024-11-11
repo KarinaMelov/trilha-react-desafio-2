0b078a50d268d24c2e7e2c7813be029a4fb26320
Karina Vieira <karinamelov@gmail.com>
2024-11-11 09:35:59 -0300

    Incrementando função para remover repositório via Id

--- src/pages/App.js
+++ src/pages/App.js
@@ -35,8 +35,9 @@ function App() {
 
   const handleRemoveRepo = (id) => {
     console.log('Removendo registro', id);
+    const updateRepos = repos.filter(repo => repo.id !== id);
+    setRepos(updateRepos);
 
-    // utilizar filter.
   }
 
 
